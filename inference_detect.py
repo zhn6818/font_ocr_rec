ee63156488395f40e25bcd482e8fe87f3751e1f7
张海宁 <zhn6818@163.com>
2024-11-04 10:48:32 +0800

    Update inference_detect.py

--- inference_detect.py
+++ inference_detect.py
@@ -23,6 +23,28 @@ image_path = 'test.png'  # 替换为你的图片路径
 
 horizontal_boxes, free_boxes = detector.detect_img(image_path, text_threshold=0.7, low_text=0.4, link_threshold=0.4)
 
+def crop_image_aspect_ratio(image, aspect_ratio=5):
+    """根据给定的宽高比裁剪图像，保持宽高比为5:1"""
+    img_height, img_width = image.shape[:2]
+    
+    # 每个小图的宽度和高度
+    crop_height = img_height
+    crop_width = int(crop_height * aspect_ratio)
+    
+    # 如果宽度超过原始图像宽度，则进行调整
+    if crop_width > img_width:
+        crop_width = img_width
+    
+    crops = []
+    
+    # 从图像中裁剪出多个5:1比例的图像
+    for start_x in range(0, img_width, crop_width):
+        end_x = start_x + crop_width
+        crop = image[:, start_x:end_x]
+        if crop.shape[1] > 0:
+            crops.append(crop)
+    
+    return crops
 
 # 加载图片
 img = cv2.imread(image_path)
@@ -40,6 +62,8 @@ for box in horizontal_boxes[0]:
     cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
 
     img_crop = img[y_min:y_max, x_min:x_max]
+    cropped_images = crop_image_aspect_ratio(img_crop)
+
     result_crop = reader.read_fulltext(img_crop, output_format='dict')
 
     all_results.extend(result_crop)
