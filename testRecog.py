91ce6c1b3b94ff7a297c55f5a9d4bc52f8f34e04
张海宁 <zhn6818@163.com>
2024-11-06 08:46:05 +0800

    update

--- testRecog.py
+++ testRecog.py
@@ -1,10 +1,16 @@
 import requests
+import argparse
+
+# 创建命令行参数解析器
+parser = argparse.ArgumentParser(description="上传图片并请求预测")
+parser.add_argument('image_path', type=str, help="输入要上传的图片路径")
+args = parser.parse_args()
 
 # 设置Flask服务器地址
 url = 'http://127.0.0.1:5000/predict'
 
 # 打开你想上传的图像
-with open('test1.png', 'rb') as f:
+with open(args.image_path, 'rb') as f:
     files = {'file': f}
     response = requests.post(url, files=files)
 
